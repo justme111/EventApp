288f927985bc86a9db3a0db3b1cd5b4daeb3184c
justme111 <nenji91@web.de>
2017-04-20 13:27:51 +0200

    changes

---
+++ EventPlaning/app/src/main/java/GLDev/eventplaning/app/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package GLDev.eventplaning.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Michi on 20.04.2017.
+ */
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+
+}
