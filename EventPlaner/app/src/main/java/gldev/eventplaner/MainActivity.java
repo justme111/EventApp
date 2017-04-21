b8aa8dcc111204f993f45d04a3bea6deafaf4da5
justme111 <nenji91@web.de>
2017-04-21 14:22:25 +0200

    design

---
+++ EventPlaner/app/src/main/java/gldev/eventplaner/MainActivity.java
@@ -0,0 +1,16 @@
+package gldev.eventplaner;
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
